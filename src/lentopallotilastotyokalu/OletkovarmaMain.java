30f40e60c2071f850a7adce019baea6ee78b4aac
Inkilä Reetu Lauri Taavetti <lauri.rt.inkila@student.jyu.fi>
2021-01-31 18:15:28 +0200

    Varoitus poistosta

---
+++ src/lentopallotilastotyokalu/OletkovarmaMain.java
@@ -0,0 +1,38 @@
+package lentopallotilastotyokalu;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+
+
+/**
+ * @author RInkila
+ * @version 31.1.2021
+ *
+ */
+public class OletkovarmaMain extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader ldr = new FXMLLoader(getClass().getResource("OletkovarmaGUIView.fxml"));
+            final Pane root = ldr.load();
+            //final OletkovarmaGUIController oletkovarmaCtrl = (OletkovarmaGUIController) ldr.getController();
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("oletkovarma.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Oletkovarma");
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args Ei käytössä
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
