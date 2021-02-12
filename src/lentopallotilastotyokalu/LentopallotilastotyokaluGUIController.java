04eecf2aa80684ddf7f87a771414cf085072fbe4
Inkilä Reetu Lauri Taavetti <lauri.rt.inkila@student.jyu.fi>
2021-02-12 13:19:43 +0200

    Ensimmäiset ei osata ikkunat

--- src/lentopallotilastotyokalu/LentopallotilastotyokaluGUIController.java
+++ src/lentopallotilastotyokalu/LentopallotilastotyokaluGUIController.java
@@ -1,10 +1,24 @@
 package lentopallotilastotyokalu;
 
+import fi.jyu.mit.fxgui.Dialogs;
+import javafx.fxml.FXML;
 /**
  * @author RInkila
- * @version 29.1.2021
+ * @version Feb 9, 2021
  *
  */
 public class LentopallotilastotyokaluGUIController {
-      // TODO
+
+    @FXML void handleLisaaPelaaja() {
+        Dialogs.showMessageDialog("Ei osata lisätä pelaajaa");
+        // TODO: korvaa muokkauksella
+        
+    }
+    
+    @FXML void handlePoistaPelaaja() {
+        Dialogs.showMessageDialog("Ei osata poistaa pelaajaa");
+        // TODO: korvaa muokkauksella
+        
+    }
+
 }
